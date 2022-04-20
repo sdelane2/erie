f99ca417c49b6f2ff01fe85836b1e52442a8417b
sdelane2 <seanmartindelaney@gmail.com>
2022-04-20 01:36:14 -0400

    Added rspec-rails and factorybot gems

---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Books API", type: :request do
+    it "returns all books" do
+        FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+        get '/api/v1/books'
+        expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body).size).to be > 0
+    end
+end
